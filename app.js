a7df859e3d3074965c9b15c5fd0a62e5e6a8c14e
Lauren Leohner <laurenleohner@gmail.com>
2017-05-02 17:01:44 -0400

    adding more stuff

--- app.js
+++ app.js
@@ -1,16 +1,32 @@
 (function() {
-var app = angular.module("myApp", []);
+var app = angular.module('myApp', []);
 
-$scope.addWord = function(newWord){
+    app.controller('ourController', function($scope){
 
-}
-words: [{text:"yuck!"},
-        {text:"mommy!"},
-        {text:"daddy!"},
-        {text:"are we there yet?"}
-        {text:"why?"},
-        {text:"he hit me"},
-        {text:"ouch!"}
-       ]
+    $scope.wordList = [];
+    $scope.phraseList = [];
 
+    $scope.addWord = function(){
+      var item = $scope.words[Math.floor(Math.random()*$scope.words.length)];
+      $scope.wordList.push( item );
+      }
+
+
+// need to change this function to pull more words out of array
+      $scope.addWordMore = function(){
+        var item = $scope.words[Math.floor(Math.random()*$scope.words.length)];
+        $scope.phraseList.push( item );
+        }
+
+
+
+      $scope.words = [{text: 'yuck!'},
+              {text: 'mommy!'},
+              {text: 'daddy!'},
+              {text: 'are we there yet?'},
+              {text: 'why?'},
+              {text: 'he hit me'},
+              {text: 'ouch!'}
+            ];
+})
 })();
