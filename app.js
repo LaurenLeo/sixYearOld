7a7bc8d2f51b312cd7662a5b9710ae6dcde74064
Mark Upson <markaupson@gmail.com>
2017-05-02 17:30:54 -0400

    app.js changes

--- app.js
+++ app.js
@@ -14,19 +14,29 @@ var app = angular.module('myApp', []);
 
 // need to change this function to pull more words out of array
       $scope.addWordMore = function(){
-        var item = $scope.words[Math.floor(Math.random()*$scope.words.length)];
+        var item = $scope.sentences[Math.floor(Math.random()*$scope.sentences.length)];
         $scope.phraseList.push( item );
         }
-
+      $scope.sentences = [{text: 'are we there yet?'},
+                          {text: 'he hit me'},
+                          {text: 'I do not want to'},
+                          {text: 'She hit me'},
+                          {text: 'I think its dead'},
+                          {text: 'I pooped'},
+                          {text: 'I am not tired'},
+                          {text: 'What is that?'}
+                          ]
 
 
       $scope.words = [{text: 'yuck!'},
               {text: 'mommy!'},
               {text: 'daddy!'},
-              {text: 'are we there yet?'},
               {text: 'why?'},
-              {text: 'he hit me'},
-              {text: 'ouch!'}
+              {text: 'ouch!'},
+              {text: 'wah!'},
+              {text: 'No!'},
+              {text: 'Yay'},
+              {text: 'Poop'}
             ];
 })
 })();
