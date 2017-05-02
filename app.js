c185b8e6abd9fa480fa7b1933ca5761d7e98ee65
Mark Upson <markaupson@gmail.com>
2017-05-02 14:58:26 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,16 @@
+(function() {
+var app = angular.module("myApp", []);
+
+$scope.addWord = function(newWord){
+
+}
+words: [{text:"yuck!"},
+        {text:"mommy!"},
+        {text:"daddy!"},
+        {text:"are we there yet?"}
+        {text:"why?"},
+        {text:"he hit me"},
+        {text:"ouch!"}
+       ]
+
+})();
